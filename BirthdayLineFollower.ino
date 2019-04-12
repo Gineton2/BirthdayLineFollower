b11e7079e4159027c8d8d2d19167d05499acbf25
Gineton Alencar <gineton2@gmail.com>
2019-04-11 20:29:58 -0700

    Make calibration code run via switch toggle

--- BirthdayLineFollower.ino
+++ BirthdayLineFollower.ino
@@ -42,7 +42,8 @@ uint16_t sensorValues[SensorCount];
 #define DIRB 4 // Direction control for motor B
 #define PWMB 11 // PWM control (speed) for motor B
 
-#define ON_OFF 2 // on off switch
+#define ON_OFF 13 // on off switch
+#define CALIBRATOR 12 //calibration switch
 
 // speed consts
 #define MULTIPLIER .75
@@ -63,55 +64,32 @@ void setup()
 
   delay(500);
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
 
-  //pinMode(ON_OFF, INPUT);
-  
-//  // analogRead() takes about 0.1 ms on an AVR.
-//  // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
-//  // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
-//  // Call calibrate() 400 times to make calibration take about 10 seconds.
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    qtr.calibrate();
-  }
-  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
 
-//  // print the calibration minimum values measured when emitters were on
-  Serial.begin(9600);
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.minimum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  delay(1000);
   setupArdumoto(); // Set all pins as outputs
 }
 
 void loop()
 {
+  if ( digitalRead(CALIBRATOR) ) {
+    calibrateSensors();
+  }
   uint16_t position = sensorLoop();
 
-
-  if (position < 1500 && position > 1100) {
-    driveArdumoto(MOTOR_R, FORWARD, (255*MULTIPLIER));
-    driveArdumoto(MOTOR_L, FORWARD, (255*MULTIPLIER));
-  } else if (position < 1100) {
-    driveArdumoto(MOTOR_R, FORWARD, (255*MULTIPLIER));
-    driveArdumoto(MOTOR_L, FORWARD, (0*MULTIPLIER));
+  if (digitalRead(ON_OFF)) {
+    if (position < 1500 && position > 1100) {
+      driveArdumoto(MOTOR_R, FORWARD, (255*MULTIPLIER));
+      driveArdumoto(MOTOR_L, FORWARD, (255*MULTIPLIER));
+    } else if (position < 1100) {
+      driveArdumoto(MOTOR_R, FORWARD, (255*MULTIPLIER));
+      driveArdumoto(MOTOR_L, FORWARD, (0*MULTIPLIER));
+    } else {
+      driveArdumoto(MOTOR_R, FORWARD, 0*MULTIPLIER);
+      driveArdumoto(MOTOR_L, FORWARD, 255*MULTIPLIER);
+    }
   } else {
-    driveArdumoto(MOTOR_R, FORWARD, 0*MULTIPLIER);
-    driveArdumoto(MOTOR_L, FORWARD, 255*MULTIPLIER);
+    stopArdumoto(MOTOR_R);
+    stopArdumoto(MOTOR_L);
   }
 
 }
@@ -146,6 +124,7 @@ uint16_t sensorLoop()
     Serial.print('\t');
   }
   Serial.println(position);
+  Serial.println("Switch: " + digitalRead(ON_OFF));
 
   //delay(250);
 
@@ -174,4 +153,37 @@ void setupArdumoto()
   digitalWrite(PWMB, LOW);
   digitalWrite(DIRA, LOW);
   digitalWrite(DIRB, LOW);
+}
+
+void calibrateSensors() {
+  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
+  // analogRead() takes about 0.1 ms on an AVR.
+  // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
+  // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
+  // Call calibrate() 400 times to make calibration take about 10 seconds.
+//  for (uint16_t i = 0; i < 40; i++)
+//  {
+//    qtr.calibrate();
+//  }
+  qtr.calibrate();
+  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
+
+//  // print the calibration minimum values measured when emitters were on
+  Serial.begin(9600);
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+  // print the calibration maximum values measured when emitters were on
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
+  //delay(1000);
 }
